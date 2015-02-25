a2d748662db321846e8968efc5275e50fde0b05e
Juan Lulkin <juan.lulkin@klarna.com>
2015-02-25 21:09:09 +0200

    Initial game

---
+++ src/js/states/menu.js
@@ -0,0 +1,26 @@
+var Menu = function () {
+  this.text = null;
+};
+
+module.exports = Menu;
+
+Menu.prototype = {
+
+  create: function () {
+    var x = this.game.width / 2;
+    var y = this.game.height / 2;
+
+    var style = { font: "40px Arial", fill: "#ff0000", align: "center" };
+
+    this.text = this.add.text(0, y - 200, "Enter World of Darkness", style);
+
+    this.input.onDown.add(this.onDown, this);
+  },
+
+  update: function () {
+  },
+
+  onDown: function () {
+    this.game.state.start(playerState.currentLevel);
+  }
+};
